34bb886772cfe88379e7ccaf888d7204e862af23
panha <khepanha18@kit.edu.kh>
2021-01-18 21:35:35 +0700

    :sparkles: New Commit

---
+++ scenes/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Scene,Word,Bookmark, Understood
+
+class SceneSerializer(serializers.ModelSerializer):
+    words = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    class Meta:
+        fields = "__all__"
+        model = Scene
+class WordSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Word
+class BookmarkSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Bookmark
+class UnderstoodSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = "__all__"
+        model = Bookmark
+# class RecommendationSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         fields = "__all__"
+#         model = Recommendation
